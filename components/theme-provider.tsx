aaa25c87bc193adbe71e1390415b8b6c3be7db64
Gurvak-Singh <gurvaksingh1998@gmail.com>
2025-07-19 14:05:15 +0530

    Remove unused VSCode configuration files and enhance ThemeProvider component for improved rendering

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -5,7 +5,16 @@ import {
   ThemeProvider as NextThemesProvider,
   type ThemeProviderProps,
 } from 'next-themes'
+import { useEffect, useState } from 'react';
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return <>{children}</>;
+  
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
